f74e4fe3beaae74c43cc26519a7056209c475060
Keyvan Mahmoodzadeh Kani <arasanjkk@gmail.com>
2024-08-30 22:13:23 -0700

    Dynamic scene switching functionality
    - Updated the splash, main menu, and gameplay scene classes to become object oriented
    
    - Added run methods to each scene class in order to run respective scenes during gameplay
    
    - Added a game state manager class that keeps track of keys to different states
    
    - Added a list of states dictionary in the Main class that maps keys to each scene variable in order to help the game state manager dynamically switch between different scenes

--- Main.py
+++ Main.py
@@ -1,19 +1,40 @@
-#Imports pygame libraries
+#Imports pygame libraries and needed classes from their respective modules
 import pygame
-import sys
+from GameStateManager import GameStateManager
+from Scenes.SplashScene import SplashScene
+from Scenes.MainMenuScene import MainMenuScene
+from Scenes.GameplayScene import GameplayScene
 
 #Initializes all pygame modules
 pygame.init()
 
-#Creates display surface and sets its caption
+#Creates display surface and sets the game's caption
 WINDOW_WIDTH = 480
 WINDOW_HEIGHT = 640 
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption('Pac-Man! - Keyvan M. Kani Pygame project')
+pygame.display.set_caption('Pac-Man - Keyvan M. Kani Pygame Project')
 
 #Creates surface object image and setting it as the game's icon
 game_icon_image = pygame.image.load('Images/Player/pacman_icon.png')
 pygame.display.set_icon(game_icon_image)
+
+#A constant variable and a Clock object to keep track of the FPS
+FPS = 60
+clock = pygame.time.Clock()
+
+'''
+Sets up the game state manager to keep track of different scenes during gameplay through
+keeping track of states through keys 
+'''
+game_state_manager = GameStateManager('Splash Screen')
+
+#Sets up scenes of the game
+splash_scene = SplashScene(display_surface, game_state_manager)
+main_menu_scene = MainMenuScene(display_surface, game_state_manager)
+gameplay_scene = GameplayScene(display_surface, game_state_manager)
+
+#A dictionary that matches keys with their respective scenes to help the game state manager switch between screens of the game
+list_of_states = {'Splash Screen':splash_scene, 'Main Menu Screen':main_menu_scene, 'Gameplay Screen':gameplay_scene}
 
 #A game loop to run the game
 running = True
@@ -23,9 +44,14 @@ while running:
         #Checks if the player quit the game
         if(event.type == pygame.QUIT):
             running = False
+
+        list_of_states[game_state_manager.get_state()].run()
         
         #Constantly updates the display surface for any changes in runtime (sounds, blitting of images and text, etc.)
         pygame.display.update()
+
+        #Delays the game loop so that all computers can run at 60 frames per second for every iteration of the game loop
+        clock.tick(FPS)
     
 #Un-initializes all pygame modules
 pygame.quit()
---
+++ Scenes/GameplayScene.py
@@ -0,0 +1,7 @@
+class GameplayScene:
+    def __init__(self, display_surface, game_state_manager):
+        self.display_surface = display_surface
+        self.game_state_manager = game_state_manager
+
+    def run(self):
+        self.display_surface.fill('blue')
