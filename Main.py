547896e856ff64d9432fcc06614c32e8976a1b38
Keyvan Mahmoodzadeh Kani <arasanjkk@gmail.com>
2024-10-04 19:53:45 -0700

    Finished temporary splash scene
    - Finished the splash scene to dynamically change transparency and switch to the main menu scene
    
    - Renamed all words in the code from "screen" to "scene"

--- Main.py
+++ Main.py
@@ -13,15 +13,15 @@ from Scenes.GameplayScene import GameplayScene
 #Initializes all pygame modules
 pygame.init()
 
-#Creates display surface and sets the game's caption
+#Creates display surface
 WINDOW_WIDTH = 480
 WINDOW_HEIGHT = 640 
-display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption('Pac-Man - Keyvan M. Kani Pygame Project')
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.SRCALPHA) 
 
-#Creates surface object image and setting it as the game's icon
+#Sets the game's icon and caption
 game_icon_image = pygame.image.load('Images/Game Icon/pacman_icon.png')
 pygame.display.set_icon(game_icon_image)
+pygame.display.set_caption('Pac-Man - Keyvan M. Kani Pygame Project')
 
 #A constant variable and a Clock object to keep track of the FPS
 FPS = 60
@@ -31,15 +31,15 @@ clock = pygame.time.Clock()
 Sets up the game state manager to keep track of different scenes during gameplay through
 keeping track of states through keys 
 '''
-game_state_manager = GameStateManager('Splash Screen')
+game_state_manager = GameStateManager('Splash Scene')
 
 #Sets up scenes of the game
 splash_scene = SplashScene(display_surface, game_state_manager, WINDOW_WIDTH, WINDOW_HEIGHT)
 main_menu_scene = MainMenuScene(display_surface, game_state_manager)
 gameplay_scene = GameplayScene(display_surface, game_state_manager)
 
-#A dictionary that matches keys with their respective scenes to help the game state manager switch between screens of the game
-list_of_states = {'Splash Screen':splash_scene, 'Main Menu Screen':main_menu_scene, 'Gameplay Screen':gameplay_scene}
+#A dictionary that matches keys with their respective scenes to help the game state manager switch between scenes of the game
+list_of_states = {'Splash Scene':splash_scene, 'Main Menu Scene':main_menu_scene, 'Gameplay Scene':gameplay_scene}
 
 #A game loop to run the game
 running = True
@@ -51,14 +51,13 @@ while running:
             running = False
 
     '''
-    Contionously runs the current scene the game state manager focuses on based on player events Splash Screen
-        Note: Runs the splash screen when opening the game for the first time
+    Contionously runs the current scene the game state manager focuses on based on player events Splash Scene
+        Note: Runs the splash scene when opening the game for the first time
     '''
     list_of_states[game_state_manager.get_state()].run()
 
     #Constantly updates the display surface for any changes in runtime (sounds, blitting of images and text, etc.)
     pygame.display.update()
-    pygame.display.flip()
 
     #Delays the game loop so that all computers can run at 60 frames per second for every iteration of the game loop
     clock.tick(FPS)
--- Scenes/SplashScene.py
+++ Scenes/SplashScene.py
@@ -1,71 +1,104 @@
 '''
-Description: The SplashScene class showcases the splash screen of the game
+Description: A class to create and showcase the splash scene of the game
 '''
 
 #Imports pygame libraries
 import pygame
 
 class SplashScene:
-    #A Constructor to initialize an instance of Splash Screen class
+    #A Constructor to initialize an instance of Splash Scene class
     def __init__(self, display_surface, game_state_manager, window_width, window_height):
-        #Sets up variables relating to the Display Surface
+        #Initializes variables relating to the sisplay surface and game state manager
         self.display_surface = display_surface
         self.game_state_manager = game_state_manager
         self.WINDOW_WIDTH = window_width
         self.WINDOW_HEIGHT = window_height
 
         '''
-        Sets up the the splash surface variable to blit onto the Display Surface
+        Sets up the the splash scene surface variable to blit onto the display surface
             Note: pygame.SRCALPHA must be included in order to access the alpha channel in order to change
                   the transparency of the surface object
         '''
         self.splash_surface = pygame.Surface((self.WINDOW_WIDTH, self.WINDOW_HEIGHT), pygame.SRCALPHA)
 
-        #Sets up a variable to keep track of the transparency of the Splash Surface
+        #Initializes up variables to keep track of the transparency of the Splash Surface
         self.transparency = 0
-    
-    #A method to run the Splash Screen
-    def run(self):
-        #Sets up the Splash Surface
-        self.set_up_splash_surface()
-        
-        #An if statement to dynamically change the transparency of the splash screen
-        if self.transparency < 255:
-            self.transparency += 1
-            pygame.time.delay(80)
+        self.fully_transparent = False
 
-        #Sets the alpha of Splash Surface based on the current transparency value
+        #Initializes a variable 
+    
+    #A method to run the Splash Scene
+    def run(self):
+        #Resets the display surface background to blit the splash surface
+        self.display_surface.fill('black')
+        
+        #A method to set up the splash surfaces
+        self.set_up_splash_surface()
+
+        '''
+        An if statement to check if the transparency has reached 250 to pause the splash scene (momentarily stops
+        changing the transparency of the splash surface)
+        '''
+        if self.transparency == 250:
+            self.fully_transparent = True
+
+            #Debug code
+                #print(str(self.transparency) + " First if statement ran \n")
+
+            pygame.time.delay(3000)
+        
+        #An if-else statement to dynamically change the value of the transparency varuable for the splash surface
+        if self.fully_transparent == False:
+            self.transparency += 10
+            pygame.time.delay(50)
+
+            #Debug code
+                #print(str(self.transparency) + " Second if statement ran \n")
+        elif(self.fully_transparent and self.transparency > 0):
+            self.transparency -= 10
+            pygame.time.delay(50)
+
+            #Debug code
+                #print(str(self.transparency) + " Third if statement ran \n")
+            
+        #Sets the alpha of splash surface based on the current transparency value
         self.splash_surface.set_alpha(self.transparency)
 
-        #Blits the fully finished Splash Surface onto the Display Surface
+        #Blits the fully finished splash surface onto the display surface
         self.display_surface.blit(self.splash_surface, (0,0))
 
-        #Grabs all the keys pressed by the player
-        keys = pygame.key.get_pressed()
+        '''
+        An if statement that uses the game state manager to re-direct the player to the 
+        main menu scene after the splash scene dissapears
+            Note: Although the value of transparency is 0 in the beginning, the game 
+                  doesn't switch scenes because the variable gets incremented before
+                  reaching this if statement
+        '''
+        if self.transparency == 0:
+            self.game_state_manager.set_state('Main Menu Scene')
 
-        #Switches to the main menu screen for debugging purposes
-        if(keys[pygame.K_m]):
-            self.game_state_manager.set_state('Main Menu Screen')
+        #A method to check for player input
+        self.player_input()
 
     #A method to set up and blit the surface objects onto the Splash Surface
     def set_up_splash_surface(self):
         #Fills the background color of the Splash Surface
         self.splash_surface.fill('black')
-
-        #Sets up color for the splash screen borders and text
+        
+        #Sets up color for the splash scene borders and text
         DARK_YELLOW = (241, 196, 15)
 
-        #Sets up Splash Screen border -> Rectangle(surface, color, (top-left x, top-left y, width, height), border line width [has no fill if you define width])
+        #Sets up Splash Scene border -> Rectangle(surface, color, (top-left x, top-left y, width, height), border line width [has no fill if you define width])
         pygame.draw.rect(self.splash_surface, DARK_YELLOW, (0, 0, self.WINDOW_WIDTH, self.WINDOW_HEIGHT), 4)
 
         #Sets up Pac-Man icon by creating surface image, getting rect of the image, and positioning the rect
-        game_icon_image = pygame.image.load('Images/Splash Screen/pacman_icon.png')
+        game_icon_image = pygame.image.load('Images/Splash Scene/pacman_icon.png')
         game_icon_image = pygame.transform.scale(game_icon_image, (300, 300))
         game_icon_rect = game_icon_image.get_rect()
         game_icon_rect.centerx = self.WINDOW_WIDTH / 2
         game_icon_rect.centery = self.WINDOW_HEIGHT / 2 + 100
 
-        #Sets up Splash Screen text (title) by creating a render (surface object) of the text 
+        #Sets up Splash Scene text (title) by creating a render (surface object) of the text 
         pacmania_font = pygame.font.Font('Fonts/Pacmania/Pacmania-Normal.ttf', 96) #Loads the font
         
         splash_text = pacmania_font.render('SPLASH', True, DARK_YELLOW) #(*text*, *anti-aliasing boolean*, *color of text*, *background color of text* [optional])
@@ -73,12 +106,21 @@ class SplashScene:
         splash_text_rect.centerx = self.WINDOW_WIDTH / 2
         splash_text_rect.centery = 100
 
-        screen_text = pacmania_font.render('SCREEN', True, DARK_YELLOW)
-        screen_text_rect = screen_text.get_rect()
-        screen_text_rect.centerx = self.WINDOW_WIDTH / 2
-        screen_text_rect.centery = 200
+        scene_text = pacmania_font.render('SCREEN', True, DARK_YELLOW)
+        scene_text_rect = scene_text.get_rect()
+        scene_text_rect.centerx = self.WINDOW_WIDTH / 2
+        scene_text_rect.centery = 200
 
         #Blits game icon and "Splash" and "Text" onto the display surface
         self.splash_surface.blit(game_icon_image, game_icon_rect)
         self.splash_surface.blit(splash_text, splash_text_rect)
-        self.splash_surface.blit(screen_text, screen_text_rect)
+        self.splash_surface.blit(scene_text, scene_text_rect)
+
+    #A method to check for player input
+    def player_input(self):
+        #Grabs all the keys pressed by the player
+        keys = pygame.key.get_pressed()
+
+        #Switches to the main menu scene for debugging purposes
+        if(keys[pygame.K_m]):
+            self.game_state_manager.set_state('Main Menu Scene')
